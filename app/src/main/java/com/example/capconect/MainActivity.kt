ce6aef36d06f9e2d9edb3b36d96862ffee4c9694
LONE WOLF <johnkellyjoseph777@gmail.com>
2025-11-05 19:06:26 -0500

    fix

--- app/src/main/java/com/example/capconect/MainActivity.kt
+++ app/src/main/java/com/example/capconect/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.capconect
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity_layout)
+        textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Hello World"
     }
 }
